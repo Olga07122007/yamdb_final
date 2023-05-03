cc28530f73a1d89926cf76158fc2161057bf86f9
Olga Yagnyuk <olga07122007@yandex.ru>
2023-05-03 19:15:50 +0300

    key22

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -12,7 +12,7 @@ SECRET_KEY = os.getenv(
     'SECRET_KEY',
     default='p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs')
 
-SECRET_KEY = str(os.getenv('SECRET_KEY'))
+#SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
