20eb57dad17c3e8245b67af03b6f2e8c952692a6
Olga Yagnyuk <olga07122007@yandex.ru>
2023-05-05 19:11:00 +0300

    readme10

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -177,5 +177,6 @@ class CommentViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         review_id = self.kwargs.get('review_id')
+
         review = get_object_or_404(Review, pk=review_id)
         serializer.save(author=self.request.user, review=review)
